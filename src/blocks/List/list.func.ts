71327085abb78e1b95300b6406ed8e0c703ede5c
Abdessamad El Hamdany <abdessamadelhamdany@hotmail.com>
2024-12-13 22:27:56 +0100

    persist selection/focus, and support withShift

--- src/blocks/List/list.func.ts
+++ src/blocks/List/list.func.ts
@@ -1,18 +1,28 @@
-import { logProxies, uniqueId } from '@/core'
-import { List, ListItem } from '@/types'
+import { uniqueId } from '@/core'
+import { List, ListItem, MarkdownerElement } from '@/types'
 
-const activateListItemElement = (id: string) => {
+const activateListItemElement = (
+  id: string,
+  selection?: {
+    selectionStart: number
+    selectionEnd: number
+  },
+) => {
   setTimeout(() => {
-    const element = document.getElementById(id)
-    element?.focus()
+    const element = document.getElementById(id) as MarkdownerElement
+    element.setSelectionRange(
+      selection ? selection.selectionStart : element.value.length,
+      selection ? selection.selectionEnd : element.value.length,
+    )
+    element.focus()
   })
 }
 
 const getListItemMeta = (items: ListItem[], path: string) => {
   let idx = -1
-  let item: ListItem | undefined
-  let parent: ListItem | undefined
+  let item: ListItem = { id: '', text: '' }
   let isParent = false
+  const parents: ListItem[] = []
   const lastItemId = path.split('/').slice(-1).pop()
 
   for (const itemId of path.split('/')) {
@@ -37,16 +47,16 @@ const getListItemMeta = (items: ListItem[], path: string) => {
     }
 
     if (itemId !== lastItemId) {
-      parent = foundItem
+      parents.push(foundItem)
     }
   }
 
-  return { idx, item: item as ListItem, parent: parent as ListItem, isParent }
+  return { idx, item, parents, isParent }
 }
 
 export const addListItem = (draftBlock: List, path: string) => {
   const newItem = { id: uniqueId(), text: '' }
-  const { idx, item, parent, isParent } = getListItemMeta(
+  const { idx, item, parents, isParent } = getListItemMeta(
     draftBlock.content.items,
     path,
   )
@@ -57,8 +67,10 @@ export const addListItem = (draftBlock: List, path: string) => {
     return
   }
 
-  parent.subItems?.items.splice(idx + 1, 0, newItem)
-  activateListItemElement(newItem.id)
+  for (const parent of parents.slice(-1)) {
+    parent.subItems?.items.splice(idx + 1, 0, newItem)
+    activateListItemElement(newItem.id)
+  }
 }
 
 export const nestListItem = (
@@ -66,22 +78,38 @@ export const nestListItem = (
   path: string,
   withShift: boolean,
 ) => {
-  const { idx, item, parent, isParent } = getListItemMeta(
-    draftBlock.content.items,
-    path,
-  )
+  const { idx, item, parents } = getListItemMeta(draftBlock.content.items, path)
+  const { selectionStart, selectionEnd } = document.getElementById(
+    item.id,
+  ) as MarkdownerElement
 
-  const prevItem = parent.subItems && parent.subItems.items[idx - 1]
-  logProxies({ parent, prevItem, item })
-
-  if (parent && prevItem) {
-    parent.subItems?.items.splice(idx, 1)
-    if (prevItem.subItems) {
-      prevItem.subItems.items.push(item)
-    } else {
-      prevItem.subItems = {
-        type: 'unordered',
-        items: [item],
+  for (const parent of parents.slice(-1)) {
+    if (withShift) {
+      for (const grandParent of parents.slice(-2, -1)) {
+        if (grandParent.subItems) {
+          for (let i = 0; i < grandParent.subItems.items.length; i++) {
+            if (grandParent.subItems.items[i].id === parent.id) {
+              parent.subItems?.items.splice(idx, 1)
+              grandParent.subItems.items.splice(i + 1, 0, item)
+              activateListItemElement(item.id, { selectionStart, selectionEnd })
+              break
+            }
+          }
+        }
+      }
+    } else if (parent.subItems) {
+      const prevItem = parent.subItems.items[idx - 1]
+      if (prevItem) {
+        parent.subItems.items.splice(idx, 1)
+        if (prevItem.subItems) {
+          prevItem.subItems.items.push(item)
+        } else {
+          prevItem.subItems = {
+            type: parent.subItems.type,
+            items: [item],
+          }
+        }
+        activateListItemElement(item.id, { selectionStart, selectionEnd })
       }
     }
   }
