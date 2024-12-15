4c6c67134fcc41b25a5539a371b0d6b4bc5031ac
Abdessamad El Hamdany <abdessamadelhamdany@hotmail.com>
2024-12-15 01:24:23 +0100

    add empty paragraph as default initial value

--- src/components/Markdowner.tsx
+++ src/components/Markdowner.tsx
@@ -1,8 +1,17 @@
 import { MarkdownerProps } from '@/types'
-import { MarkdownerProvider } from '@/core'
+import { MarkdownerProvider, uniqueId } from '@/core'
 import { MarkdownerToolbar, MarkdownerBlocks } from '@/components'
 
-export function Markdowner({ onUpload, initialBlocks = [] }: MarkdownerProps) {
+export function Markdowner({
+  onUpload,
+  initialBlocks = [
+    {
+      id: uniqueId(),
+      text: '',
+      type: 'paragraph',
+    },
+  ],
+}: MarkdownerProps) {
   return (
     <MarkdownerProvider value={{ initialBlocks, onUpload }}>
       <div className="relative flex flex-col space-y-6">
